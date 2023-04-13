a64f76247fc6542be350683fd98d6e107431e12e
MutsukiOri <57990154+MutsukiOri@users.noreply.github.com>
2023-04-13 14:04:32 +0900

    add ログ出力

--- index.js
+++ index.js
@@ -34,9 +34,11 @@ async function handleEvent(event) {
     return Promise.resolve(null);
   }
 
+  console.log(event)
+
   return client.replyMessage(event.replyToken, {
     type: 'text',
-    text: event //実際に返信の言葉を入れる箇所
+    text: event.message.text //実際に返信の言葉を入れる箇所
   });
 }
 
